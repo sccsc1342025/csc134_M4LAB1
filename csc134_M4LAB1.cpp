213122a7f827c6260c90f060330755b32098fd2e
sccsc1342025 <sebastiancam711@gmail.com>
2025-03-25 19:34:10 +0000

    csc134_M4LAB1.cpp

--- csc134_M4LAB1.cpp
+++ csc134_M4LAB1.cpp
@@ -10,14 +10,35 @@ int main()
 {
     int height, width;
 
-    height = 5;
-    width = 5;
+    // height = 5;
+    // width = 5;
+    cout << "How tall should the block be?" << endl;
+    cin >> height;
+    cout << "How wide should the block be?" << endl;
+    cin >> width;
 
+    cout << "One Row" << endl;
     for (int i=0; i < width; i++)
     {
         cout << "*" << " ";
     }
     cout << endl;
+
+    cout << "One Column" << endl;
+    for (int j=0; j < height; j++)
+    {
+        cout << "*" << endl;
+    }
+
+    cout << "The entire block" << endl;
+    for (int j=0; j < height; j++)
+    {
+        for (int i=0; i < width; i++)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
     
     return 0;
 
